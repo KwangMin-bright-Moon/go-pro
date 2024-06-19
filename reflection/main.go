42311fcac68345ef91baf7e1c056188eb3b72469
Jason <jasondal91@gmail.com>
2024-06-19 22:12:32 +0900

    reflection value features exam

--- reflection/main.go
+++ reflection/main.go
@@ -1,26 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"reflect"
-	"strings"
 )
 
 func printDetails(values ...interface{}){
 	for _, elem := range values {
-		fieldDetails := []string {}
-		elemType := reflect.TypeOf(elem)
 		elemValue := reflect.ValueOf(elem)
-		if elemType.Kind() == reflect.Struct {
-			for i := 0; i < elemType.NumField(); i++ {
-				fieldName := elemType.Field(i).Name
-				fieldVal := elemValue.Field(i)
-				fieldDetails = append(fieldDetails, 
-					fmt.Sprintf("%v: %v", fieldName, fieldVal))
+		switch elemValue.Kind() {
+		case reflect.Bool:
+			var val bool = elemValue.Bool()
+			Printfln("Bool: %v", val)
+		case reflect.Int:
+			var val int64 = elemValue.Int()
+			Printfln("Int: %v", val)
+		case reflect.Float32, reflect.Float64:
+			var val float64 = elemValue.Float()
+			Printfln("Float: %v", val)
+		case reflect.String:
+			var val string = elemValue.String()
+			Printfln("String: %v", val)
+		case reflect.Ptr:
+			var val reflect.Value = elemValue.Elem()
+			if (val.Kind() == reflect.Int){
+				Printfln("Pointer to int: %v", val.Int())
 			}
-			Printfln("%v: %v", elemType.Name(), strings.Join(fieldDetails, ", "))
-		} else {
-			Printfln("%v: %v", elemType.Name(), elemValue)
+		default:
+			Printfln("Other: %v", elemValue.String())
 		}
 	}
 
@@ -36,14 +42,7 @@ func main() {
 	product := Product{
 		Name: "Kayak", Category: "Watersports", Price: 279,
 	}
+	number := 100
 
-	customer := Customer{
-		Name: "Alice", City: "New York",
-	}
-
-	payment := Payment { 
-		Currency: "USD", Amount: 100.50,
-	}
-
-	printDetails(product, customer, payment, 10, true)
+	printDetails(true, 10, 23.30, "Alice", &number, product)
 }
