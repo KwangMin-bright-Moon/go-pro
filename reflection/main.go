bfd1d400b479a6f28f05f05c85fd592b864f6d7e
Jason <jasondal91@gmail.com>
2024-06-20 21:08:49 +0900

    obtaining underlying values

--- reflection/main.go
+++ reflection/main.go
@@ -1,60 +1,17 @@
 package main
 
-import (
-	"reflect"
-)
+import "reflect"
 
-var intPtrType = reflect.TypeOf((*int)(nil))
-var byteSliceType = reflect.TypeOf([]byte(nil))
-
-func printDetails(values ...interface{}){
-	for _, elem := range values {
-		elemValue := reflect.ValueOf(elem)
-		elemType := reflect.TypeOf(elem)
-		if elemType == intPtrType {
-			Printfln("Pointer to Int: %v", elemValue.Elem().Int())
-		} else if (elemType == byteSliceType) {
-			Printfln("Byte slice: %v", elemValue.Bytes())
-		} else {
-			switch elemValue.Kind() {
-			case reflect.Bool:
-				var val bool = elemValue.Bool()
-				Printfln("Bool: %v", val)
-			case reflect.Int:
-				var val int64 = elemValue.Int()
-				Printfln("Int: %v", val)
-			case reflect.Float32, reflect.Float64:
-				var val float64 = elemValue.Float()
-				Printfln("Float: %v", val)
-			case reflect.String:
-				var val string = elemValue.String()
-				Printfln("String: %v", val)
-			// case reflect.Ptr:
-			// 	var val reflect.Value = elemValue.Elem()
-			// 	if (val.Kind() == reflect.Int){
-			// 		Printfln("Pointer to int: %v", val.Int())
-			// 	}
-			default:
-				Printfln("Other: %v", elemValue.String())
-			}
-		}	
-		
+func selectValue(data interface{}, index int) (result interface{}) {
+	dataVal := reflect.ValueOf(data)
+	if dataVal.Kind() == reflect.Slice {
+		result = dataVal.Index(index).Interface()
 	}
-
+	return
 }
-
-type Payment struct {
-	Currency string
-	Amount float64
-}
-
 
 func main() {
-	product := Product{
-		Name: "Kayak", Category: "Watersports", Price: 279,
-	}
-	number := 100
-	slice := []byte("Alice")
-
-	printDetails(true, 10, 23.30, "Alice", &number, product, slice)
+	names := []string {"Alice", "Bob", "Charlie"}
+	val := selectValue(names, 1).(string)
+	Printfln("Selected: %v", val)
 }
