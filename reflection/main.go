336cccb3af00c47bd38a3384c552cc061867cb64
Jason <jasondal91@gmail.com>
2024-06-25 20:26:24 +0900

    converting values

--- reflection/main.go
+++ reflection/main.go
@@ -4,28 +4,26 @@ import (
 	"reflect"
 )
 
-func contains(slice interface{}, target interface{}) (found bool){
-	sliceVal := reflect.ValueOf(slice)
-	if sliceVal.Kind() == reflect.Slice  {
-		for i := 0; i < sliceVal.Len(); i++ {
-			if reflect.DeepEqual(sliceVal.Index(i).Interface(), target){
-				found = true
-			}
-		}
+func convert(src, target interface{}) (result interface{}, assigned bool) {
+	srcVal := reflect.ValueOf(src)
+	targetVal := reflect.ValueOf(target)
+	if srcVal.Type().ConvertibleTo(targetVal.Type()){
+		result = srcVal.Convert(targetVal.Type()).Interface()
+		assigned = true
+	} else {
+		result = src
 	}
 	return
 }
 
 
 func main() {
-	city := "London"
+	name := "Alice"
+	price := 279
 
-	citiesSlice := []string{"Paris", "Rome", "London"}
-	Printfln("Found #1: %v", contains(citiesSlice, city))
-
-	
-	sliceOfSlices := [][]string{
-		citiesSlice, {"First", "Second", "Third"}}
-	Printfln("Found #2: %v", contains(sliceOfSlices, citiesSlice))
+	newVal, ok := convert(price, 100.00)
+	Printfln("Converted %v: %v, %T", ok, newVal, newVal)
+	newVal, ok = convert(name, 100.00)
+	Printfln("Converted %v: %v, %T", ok, newVal, newVal)
 }
 
